aecf8ef40f73cb630372e0e81a9829c74f1bf5eb
Kamil <KamKur2002@gmail.com>
2023-09-22 22:03:31 +0200

    correction

--- Polish_population_structure/SQL/creating_tables.sql
+++ Polish_population_structure/SQL/creating_tables.sql
@@ -1,9 +1,11 @@
 -- Project reffering to date from polish "GUS" (Central Statistical Office) about population structure in year 2021
 
 -- creating a schema for data
+
 CREATE SCHEMA IF NOT EXISTS poland_population_structure;
 
 -- creating population structure data tables
+
 DROP TABLE IF EXISTS poland_population_structure.voivodeship;
 CREATE TABLE poland_population_structure.voivodeship (
 voivodeship_id int 
@@ -19,10 +21,12 @@ voivodeship_id int
 
 -- importing date from csv file
 
-LOAD DATA INFILE 'F:/SQL-projects/voivodeship.csv'
+LOAD DATA INFILE 'F:/SQL-projects/Polish_population_structure/CSV/voivodeship.csv'
 INTO TABLE voivodeship
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
+
+-- Creating table with data for cities with more than 100 000 inhabitants
 
 DROP TABLE IF EXISTS poland_population_structure.city;
 CREATE TABLE poland_population_structure.city (
@@ -33,25 +37,22 @@ city_id int
 ,legitimate int
 ,illegitimate int
 ,still_births int
+,population int
+,area int
+
 ,PRIMARY KEY (city_id)
 
 
 );
 
-LOAD DATA INFILE 'F:/SQL-projects/city.csv'
+-- importing date from csv file
+
+LOAD DATA INFILE 'F:/SQL-projects/Polish_population_structure/CSV/city.csv'
 INTO TABLE city
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 
-SELECT 
-   voivodeship_id, 
-  COUNT(voivodeship_id) AS count_value 
-FROM 
-  city
-GROUP BY 
-  voivodeship_id
-ORDER BY 
-  count_value DESC
+
  
   
