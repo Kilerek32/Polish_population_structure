e2d67fade816d1d8e62618b8585afea339d4b477
Kamil <KamKur2002@gmail.com>
2023-09-26 23:26:11 +0200

    update

--- source_data/SQL/Polish_population_structure.sql
+++ source_data/SQL/Polish_population_structure.sql
@@ -1,8 +1,29 @@
-Select v.voivodeship_id, v.name, c.name 
-	from voivodeship as v
-		join city as c
-			on v.voivodeship_id=c.voivodeship_id
-				order by voivodeship_id
+-- biggest polish cities by voivodeship
+
+Select v.name, c.name, c.population
+From city as c
+Join
+( 
+    Select  voivodeship_id, max(population) As population
+    From city
+    Group By voivodeship_id
+) d On c.voivodeship_id = d.voivodeship_id And c.population = d.population
+
+join voivodeship as v
+	on v.voivodeship_id=c.voivodeship_id
+	
+order by population desc;
+
+-- total births per 100000 inhabitants in Polish cities which count above 100 000 inhabitants
+
+Select name, total, population, ROUND((100000*total)/population,0) as births_per_inhabitants 
+	from city
+		order by births_per_inhabitants desc;
+        
+-- 
+
+select * from voivodeship
+
 
 
 
--- source_data/SQL/creating_tables.sql
+++ source_data/SQL/creating_tables.sql
@@ -6,6 +6,8 @@ CREATE SCHEMA IF NOT EXISTS poland_population_structure;
 
 -- creating population structure data tables
 
+-- creating table with data for births by voivodeship
+
 DROP TABLE IF EXISTS poland_population_structure.voivodeship;
 CREATE TABLE poland_population_structure.voivodeship (
 voivodeship_id int 
@@ -21,12 +23,12 @@ voivodeship_id int
 
 -- importing date from csv file
 
-LOAD DATA INFILE 'F:/SQL-projects/Polish_population_structure/CSV/voivodeship.csv'
+LOAD DATA INFILE 'F:/Polish_population_structure/source_data/CSV/voivodeship.csv'
 INTO TABLE voivodeship
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
--- Creating table with data for cities with more than 100 000 inhabitants
+-- creating table with data for births in cities with more than 100 000 inhabitants
 
 DROP TABLE IF EXISTS poland_population_structure.city;
 CREATE TABLE poland_population_structure.city (
@@ -47,12 +49,30 @@ city_id int
 
 -- importing date from csv file
 
-LOAD DATA INFILE 'F:/SQL-projects/Polish_population_structure/CSV/city.csv'
+LOAD DATA INFILE 'F:/Polish_population_structure/source_data/CSV/city.csv'
 INTO TABLE city
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
+-- creating table with data for live births by education level of Mother and voivodeship
 
+DROP TABLE IF EXISTS poland_population_structure.education_level;
+CREATE TABLE poland_population_structure.education_level (
+voivodeship_id int 
+,name text
+,higher int
+,post_secondary int
+,secondary int
+,basic_vacational int
+,lower_secondary int
+,primary_and_incomplete_primary int
 
- 
+);
+
+-- importing date from csv file
+
+ LOAD DATA INFILE 'F:/Polish_population_structure/source_data/CSV/education_level.csv'
+INTO TABLE education_level
+FIELDS TERMINATED BY ','
+IGNORE 1 ROWS;
   
